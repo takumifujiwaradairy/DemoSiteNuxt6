578f5a9b3dc84468ff7441f21cd78e6f7594c296
Your Name <you@example.com>
2022-02-25 11:07:59 +0900

    ユーザーのSeedの作成

--- Back6/db/seeds.rb
+++ Back6/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.create(title: 'AAA', body: 'AAAAAAAAAAAAAAAA', user_id: '1')
+Article.create(title: 'BBB', body: 'BBBBBBBBBBBBBBBB', user_id: '1')
+Article.create(title: 'CCC', body: 'CCCCCCCCCCCCCCCC', user_id: '1')
+
+User.create(name: 'AAA', email: 'aaa@bar.com', password: 'AAAAAA')
+User.create(name: 'BBB', email: 'bbb@bar.com', password: 'BBBBBB')
+User.create(name: 'CCC', email: 'ccc@bar.com', password: 'CCCCCC')
