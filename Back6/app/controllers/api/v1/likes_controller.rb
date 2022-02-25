9560d965e09ad3d04e45637da644ab4e84c5a244
Your Name <you@example.com>
2022-02-25 14:54:10 +0900

    お気に入りを削除できるようにする

--- Back6/app/controllers/api/v1/likes_controller.rb
+++ Back6/app/controllers/api/v1/likes_controller.rb
@@ -1,9 +1,17 @@
 class Api::V1::LikesController < ApplicationController
-  before_action :set_article, only: [:create]
+  before_action :set_article, only: [:create, :destroy]
 
   def create
     like = @article.likes.build(user_id: current_user.id)
     if like.save
+      article_count = @article.likes.count
+      render json: article_count
+    end
+  end
+
+  def destroy
+    like = @article.likes.find_by(user_id: current_user.id)
+    if like.destroy
       article_count = @article.likes.count
       render json: article_count
     end
